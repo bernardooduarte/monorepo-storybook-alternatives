4cfde4b45702ca79f2882186d5ad99dbaa874efa
Bernardo Duarte <bernardoduarte.mg@gmail.com>
2026-02-28 13:58:05 -0300

    Corrige erro de build causado por sintaxe CSS ou atualização de loaders

--- apps/playroom-react/webpack.config.js
+++ apps/playroom-react/webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
             },
 
             {
-                test: /\.css$/i,
+                test: /\.css$/,
                 use: ["style-loader", "css-loader"]
             },
 
